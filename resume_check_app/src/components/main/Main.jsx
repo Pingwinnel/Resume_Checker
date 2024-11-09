3abb13406558d58e599f6329899a39262cb153f3
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-11-09 17:14:33 +0500

    add loader show a response errors

--- resume_check_app/src/components/main/Main.jsx
+++ resume_check_app/src/components/main/Main.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import "./Main.css"
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -9,6 +9,10 @@ const Main = () => {
     const [files, setFiles] = useState("");
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [candidates, setCandidates] = useState([]);
+    const [successCount, setSuccessCount] = useState(0);
+    const [failureCount, setFailureCount] = useState(0);
+    const [failedFiles, setFailedFiles] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const submitFiles = async (e) => {
         e.preventDefault();
@@ -25,6 +29,8 @@ const Main = () => {
             formData.append("files", files[i]);
         }
 
+        setLoading(true);
+
         try {
             const response = await fetch("http://localhost:8081/api/candidates/upload", {
                 method: "POST",
@@ -33,23 +39,53 @@ const Main = () => {
             });
 
             if (response.ok) {
-                toast.success("PDF file uploaded successfully.!", {
-                    position: "top-center",
-                    autoClose:2000
-                });
+                const data = await response.json();
+
+                setSuccessCount(data.successCount || 0);
+                setFailureCount(data.failureCount || 0);
+                setFailedFiles(data.failedFiles || []);
+
+                if (data.successCount === files.length) {
+                    toast.success("All PDFs uploaded successfully", {
+                        position: "top-center",
+                        autoClose: 3000
+                    });
+                } else {
+                    toast.success(`${data.successCount} PDFs uploaded successfully`, {
+                        position: "top-center",
+                        autoClose: 3000
+                    });
+
+                    if (data.failedFiles.length > 0) {
+                        const failedFilesList = data.failedFiles.map((file, index) => (
+                            <div key={file}>{index + 1}.{file}</div>
+                        ));
+                        toast.error(<div>Failed to upload {failureCount} files:<br/>{failedFilesList}</div>, {
+                            position: "top-right",
+                            autoClose: false,
+                        });
+                    }
+                }
             } else {
                 const errorText = await response.text();
-                toast.error(errorText,{
+                toast.error(errorText, {
                     position: "top-center",
-                    autoClose:false
+                    autoClose: false
                 });
             }
         } catch (error) {
-            toast.error("Error uploading PDF(s): " + error.message,{
+            toast.error("Error uploading PDF(s): " + error.message, {
                 position: "top-center",
-                autoClose:false
+                autoClose: false
             });
+        } finally {
+            setLoading(false);
         }
+        setSelectedFiles([]);
+        setFiles("");
+
+        document.getElementById("files").value = null;
+
     };
 
 
@@ -94,33 +130,42 @@ const Main = () => {
     return (
         <div className="main-container">
             <ToastContainer/>
-            <form className="form" onSubmit={submitFiles}>
-                <input
-                    id="files"
-                    type="file"
-                    multiple={true}
-                    accept="application/pdf"
-                    required={true}
-                    onChange={handleFileChange}
-                />
-                <label className={"form-label "} htmlFor="files">
-                    <i class="fa-solid fa-arrow-up-from-bracket"></i>
-                    &nbsp; Choose Files To Upload
-                </label>
-                <div id="num-of-files">{selectedFiles.length} Files Selected</div>
-                <ul id="files-list">
-                    {selectedFiles.map((file, index) => (
-                        <li key={index}>
-                            <p>{file.name}</p>
-                            <p>{file.size}</p>
 
-                        </li>
-                    ))}
-                </ul>
-                <button className="upload_btn" type="submit" onClick={()=>setSelectedFiles([])}>Upload</button>
-            </form>
+                <form className="form" onSubmit={submitFiles}>
+                    <input
+                        id="files"
+                        type="file"
+                        multiple={true}
+                        accept="application/pdf"
+                        required={true}
+                        onChange={handleFileChange}
+                    />
+                    <label className="form-label" htmlFor="files">
+                        <i className="fa-solid fa-arrow-up-from-bracket"></i>
+                        &nbsp; Choose Files To Upload
+                    </label>
+                    <div id="num-of-files">{selectedFiles.length} Files Selected</div>
+                    {loading ?
+                        <div className='modal-container'>
+                            <div className='modal'>
+                                <div className="loader"></div>
+                                Loading
+                            </div>
+                        </div>
+                        : <ul id="files-list">
+                            {selectedFiles.map((file, index) => (
+                                <li key={index}>
+                                    <p>{file.name}</p>
+                                    <p>{file.size}</p>
+                                </li>
+                            ))}
+                        </ul>}
+
+                    <button className="upload_btn" type="submit">Upload</button>
+                </form>
         </div>
     );
 };
+
 
 export default Main;
