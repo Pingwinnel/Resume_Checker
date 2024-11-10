99d61f26e191031410ab13bda6635e8337ef216b
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-11-10 16:50:38 +0500

    fix

--- resume_check_app/src/components/pages/search/ModalFiles/ModalFiles.jsx
+++ resume_check_app/src/components/pages/search/ModalFiles/ModalFiles.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./ModalFiles.css";
 
 const ModalFiles = ({ Candidate, onClose }) => {
+        useEffect(() => {
+            const handleEsc = (event) => {
+                if (event.key === 'Escape') {
+                    onClose();
+                }
+            };
+            window.addEventListener('keydown', handleEsc);
+            return () => {
+                window.removeEventListener('keydown', handleEsc);
+            };
+        }, [onClose]);
+
     return (
                 <div className="modal__container">
                     <div className="modal-wrapper">
--- resume_check_app/src/components/pages/search/candidateFiles/CandidateFiles.jsx
+++ resume_check_app/src/components/pages/search/candidateFiles/CandidateFiles.jsx
@@ -5,34 +5,30 @@ import ModalFiles from "../ModalFiles/ModalFiles";
 const CandidateFiles = ({ results }) => {
     const [openCandidateId, setOpenCandidateId] = useState(null);
 
-    const dubug=(candidate)=>{
-        console.log(openCandidateId);
-        setOpenCandidateId(null)
-        console.log(candidate)
-    }
+    const renderCandidate = (candidate) => {
+        const [firstName, lastName] = candidate.fullName.split(" ");
+        return (
+            <div key={candidate._id} className="list__modal__item">
+                <button
+                    className="modal-show-button"
+                    onClick={() => setOpenCandidateId(candidate._id)}
+                >
+                    {firstName}, {lastName}
+                </button>
+
+                {openCandidateId === candidate._id && (
+                    <ModalFiles
+                        Candidate={candidate}
+                        onClose={() => setOpenCandidateId(null)}
+                    />
+                )}
+            </div>
+        );
+    };
 
     return (
         <div className="list__candidates__container">
-            {results && results.length > 0 && results.map((candidate) => {
-                const [firstName, lastName] = candidate.fullName.split(" ");
-                return (
-                    <div key={candidate._id} className="list__modal__item">
-                        <button
-                            className="modal-show-button"
-                            onClick={() => setOpenCandidateId(candidate._id)}
-                        >
-                            {firstName},{lastName}
-                        </button>
-
-                        {openCandidateId === candidate._id && (
-                            <ModalFiles
-                                Candidate={candidate}
-                                onClose={() => {dubug(candidate._id)}}
-                            />
-                        )}
-                    </div>
-                );
-            })}
+            {results && results.length > 0 && results.map(renderCandidate)}
         </div>
     );
 };
