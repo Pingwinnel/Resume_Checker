508b5543ae35d6c644bcc985ad504856d3571209
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-11-10 15:37:09 +0500

    Update ModalFiles.jsx

--- resume_check_app/src/components/pages/search/ModalFiles/ModalFiles.jsx
+++ resume_check_app/src/components/pages/search/ModalFiles/ModalFiles.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import "./ModalFiles.css"
-const ModalFiles = ({ModalInfoIsOpen,onClose,children}) => {
+const ModalFiles = ({ModalInfoIsOpen,Candidate,onClose,children}) => {
 
     return (
         <>
